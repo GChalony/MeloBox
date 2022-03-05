f615a1c0c27340b671e402ebb097aa6b299dbdf0
Gregoire <gchalony@mailfence.com>
2022-03-05 13:10:11 +0100

    Implement SpotifyInterface
    Removed spotifyd from logic, it will be started another way

--- src/spotify_interface.py
+++ src/spotify_interface.py
@@ -1,24 +1,41 @@
+import subprocess
+from time import sleep
+
 class SpotifyInterface:
     def setup(self):
-        self.start_spotifyd()
+        pass
     
-    def start_spotifyd(self):
-        pass
-
+    def _spotify(self, command):
+        arg = ["spotify"] + command.split(" ")
+        subprocess.run(arg)
+    
     def play(self, uri):
-        pass
+        self._spotify(f"play --uri {uri}")
 
     def pause(self):
-        pass
+        self._spotify("pause")
 
     def resume(self):
-        pass
+        self._spotify("play")
 
-    def pause_or_resume(self):
-        pass
+    def toggle(self):
+        self._spotify("toggle")
 
     def next(self):
-        pass
+        self._spotify("next")
 
     def previous(self):
-        pass
+        self._spotify("previous")
+
+
+if __name__ == "__main__":
+    s = SpotifyInterface()
+    s.setup()
+    print("Starting test")
+
+    s.play("spotify:album:5JY3b9cELQsoG7D5TJMOgw")
+    sleep(1)
+    s.toggle()
+    sleep(1)
+    s.toggle()
+    s.next()
