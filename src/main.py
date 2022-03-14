b3275e07086f662029cff889422521df39d4e2ae
Gregoire Chalony <gchalony@gmail.com>
2022-03-14 20:39:57 +0100

    Pause when removed

--- src/main.py
+++ src/main.py
@@ -10,8 +10,6 @@ hardware = Hardware()
 hardware.setup()
 
 
-hardware.led.turn_on(0, 255, 0)
-
 # Connect hardware with spotify
 hardware.next_button.register_callback(spotify.next)
 hardware.previous_button.register_callback(spotify.previous)
@@ -20,13 +18,18 @@ hardware.pause_button.register_callback(spotify.toggle)
 # Tag reading loop
 try:
     while True:
+        hardware.led.turn_on(255, 0, 0)
         hardware.nfc_reader.wait_for_tag()
         print("Tag!")
-        hardware.led.turn_on(0, 0, 255)
+        hardware.led.turn_on(0, 255, 0)
         uri = hardware.nfc_reader.read_tag()
+        hardware.led.turn_on(0, 0, 255)
         if uri is not None:
             spotify.play(uri)
+        print("waiting for tag removed")
         hardware.nfc_reader.wait_for_tag_removed()
-        hardware.led.turn_on(0, 255, 0)
+        spotify.pause()
 finally:
+    print("See ya!")
     hardware.cleanup()
+    spotify.pause()
