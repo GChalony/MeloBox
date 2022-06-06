886bb63782a31921a70e32e8079a15bc2063e7fb
Gregoire <gchalony@mailfence.com>
2022-06-06 11:36:20 +0200

    Retry to connect five times to Spotify device

--- src/spotify_interface.py
+++ src/spotify_interface.py
@@ -1,3 +1,4 @@
+import json
 import subprocess
 from time import sleep
 from utils import log
@@ -5,12 +6,31 @@ from utils import log
 class SpotifyInterface:
     @log
     def setup(self):
-        self._spotify("devices --switch-to RaspberryPi")
+        # Switch to spotifyd device, retrying a few times in case of failure
+        for i in range(5):
+            self._spotify("devices --switch-to RaspberryPi")
+            # Check if succeeded
+            try:
+                res = self._spotify("devices --verbose --raw", capture_output=True)
+                response = res.stdout.decode("utf-8")
+                print(response)
+                devices = json.loads(response)
+                for device in devices["devices"]:
+                    if device["name"] == "RaspberryPi" and device["is_active"] == True:
+                        print("Successfully changed device to RaspberryPi")
+                        return
+            except Exception as e:
+                print("Error: ", e)
+            print(f"Didn't swith device, will retry ({i} / 5)")
+            sleep(1)
+
+
+            
     
-    def _spotify(self, command):
+    def _spotify(self, command, **run_args):
         arg = ["spotify"] + command.split(" ")
         print("$", " ".join(arg))
-        subprocess.run(arg)
+        return subprocess.run(arg, **run_args)
     
     @log
     def play(self, uri):
